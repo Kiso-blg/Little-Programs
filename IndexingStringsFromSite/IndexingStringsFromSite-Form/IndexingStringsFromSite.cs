905536501336b5d3599d4b1ef7bd3658c5de2208
Kiso <kiso-blg@live.com>
2019-04-01 02:27:57 +0300

    Added Load from file button.
    Added load from .csv file. Added load from .xml file. Added load from .json file. Load from .json is subject to improvement.

--- IndexingStringsFromSite/IndexingStringsFromSite-Form/IndexingStringsFromSite.cs
+++ IndexingStringsFromSite/IndexingStringsFromSite-Form/IndexingStringsFromSite.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Linq;
@@ -31,10 +30,13 @@ namespace IndexingStringsFromSite_Form
         private Button btnToJsonFile = new Button();
         private Button btnToXmlFile = new Button();
         private Button btnToCsvFile = new Button();
+        private OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        private Button btnLoadFile = new Button();
 
         static int textLength = 11;
         static string proccessingStr = "Proccessing.....";
         static int timeCounter = 0;
+        string filePath = AppDomain.CurrentDomain.BaseDirectory;
         static Dictionary<string, int> stringsCountsDict = new Dictionary<string, int>();
 
         private void FormIndexingStringsFromSite_Load(object sender, EventArgs e)
@@ -104,7 +106,7 @@ namespace IndexingStringsFromSite_Form
             this.lblProccessing.Size = new Size(100, 20);
 
             this.tlpStringsPanel.Visible = false;
-            this.tlpStringsPanel.Font = Font = new Font("Microsoft Sans Serif", 10);
+            this.tlpStringsPanel.Font = new Font("Microsoft Sans Serif", 10);
             this.tlpStringsPanel.ColumnCount = 2;
             this.tlpStringsPanel.AutoSize = true;
             this.tlpStringsPanel.Location = new Point(15, 120);
@@ -118,6 +120,26 @@ namespace IndexingStringsFromSite_Form
             this.tlpPagesPanel.RowCount = 1;
             this.tlpPagesPanel.AutoSize = true;
 
+            this.openFileDialog1.InitialDirectory = filePath;
+            this.openFileDialog1.Title = "Browse Csv, Json or Xml file";
+            this.openFileDialog1.CheckFileExists = true;
+            this.openFileDialog1.CheckPathExists = true;
+            this.openFileDialog1.Multiselect = false;
+            this.openFileDialog1.Filter = "All types|*.json;*.xml;*.csv|" +
+                    "Json files (*.json)|*.json|Xml files (*.xm)|*.xml|Csv files (*.csv)|*.csv";
+            this.openFileDialog1.FilterIndex = 1;
+            this.openFileDialog1.RestoreDirectory = true;
+            this.openFileDialog1.ShowReadOnly = true;
+
+            this.btnLoadFile.Font = new Font("Microsoft Sans Serif", 10);
+            this.btnLoadFile.TextAlign = ContentAlignment.MiddleCenter;
+            this.btnLoadFile.Text = "Load from file";
+            this.btnLoadFile.FlatStyle = FlatStyle.Standard;
+            this.btnLoadFile.Visible = true;
+            this.btnLoadFile.Location = new Point(325, 40);
+            this.btnLoadFile.Size = new Size(120, 25);
+            this.btnLoadFile.Click += new EventHandler(BtnLoadFile_Click);
+
             Controls.Add(labelIformation);
             Controls.Add(textBoxInputWebsite);
             Controls.Add(btnDownloadSite);
@@ -127,15 +149,117 @@ namespace IndexingStringsFromSite_Form
             Controls.Add(lblProccessing);
             Controls.Add(tlpStringsPanel);
             Controls.Add(tlpPagesPanel);
+            Controls.Add(btnLoadFile);
+        }
+
+        private void BtnLoadFile_Click(object sender, EventArgs e)
+        {
+            this.timer.Start();
+            this.tlpStringsPanel.Visible = false;
+            this.tlpStringsPanel.Controls.Clear();
+            this.tlpPagesPanel.Visible = false;
+            this.tlpPagesPanel.Controls.Clear();
+            bool isBreak = false;
+
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileExtension = this.openFileDialog1
+                    .FileName
+                    .Substring(this.openFileDialog1.FileName.IndexOf('.'));
+                if (fileExtension == ".json")
+                {
+                    LoadJsonFile(ref isBreak);
+                }
+                else if (fileExtension == ".xml")
+                {
+                    LoadXmlFile(ref isBreak);
+                }
+                else if (fileExtension == ".csv")
+                {
+                    LoadCsvFile(ref isBreak);
+                }
+            }
+
+            if (!isBreak)
+            {
+                DrawTableLayoutStringsPanel(stringsCountsDict, 0);
+                DrawTableLayoutPagesPanel(stringsCountsDict.Count);
+                this.timer.Stop();
+                this.lblProccessing.Visible = false;
+                this.tlpStringsPanel.Visible = true;
+                this.tlpPagesPanel.Visible = true;
+            }
+        }
+
+        private void LoadCsvFile(ref bool isBreak)
+        {
+            try
+            {
+                stringsCountsDict = new Dictionary<string, int>();
+                using (StreamReader sr = new StreamReader(this.openFileDialog1.FileName))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string[] elements = line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        if (elements.Length == 2)
+                        {
+                            stringsCountsDict.Add(elements[0], int.Parse(elements[1]));
+                        }
+                        else
+                        {
+                            MessageBox.Show("File content does not concur to Dictionary<string, int> type!");
+                            isBreak = true;
+                            break;
+                        }
+                    }
+                }                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("File content does not concur to Dictionary<string, int> type!");
+            }
+        }
+
+        private void LoadXmlFile(ref bool isBreak)
+        {
+            try
+            {
+                XDocument xDoc = XDocument.Load(this.openFileDialog1.FileName);
+                IEnumerable<XElement> rootNodes = xDoc.Root.DescendantNodes().OfType<XElement>();
+                stringsCountsDict = rootNodes.ToDictionary(kvp => kvp.Name.ToString(), kvp => int.Parse(kvp.Value));
+            }
+            catch (Exception)
+            {
+                isBreak = true;
+                MessageBox.Show("File content does not concur to Dictionary<string, int> type!");
+            };
+        }
+
+        private void LoadJsonFile(ref bool isBreak)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(this.openFileDialog1.FileName))
+                {
+                    string jsonString = reader.ReadToEnd();
+                    stringsCountsDict = JsonConvert.DeserializeObject<Dictionary<string, int>>(jsonString);
+                }
+            }
+            catch (Exception)
+            {
+                isBreak = true;
+                MessageBox.Show("File content does not concur to Dictionary<string, int> type!");
+            }
         }
 
         private void BtnToCsvFile_Click(object sender, EventArgs e)
         {
-            string filePath = @"C:\Users\User\Desktop\IndexedStrings.csv";
             string csv = string.Join(Environment.NewLine, stringsCountsDict
                 .OrderByDescending(element => element.Value)
                 .Select(data => $"{data.Key},{data.Value},"));
-            File.WriteAllText(filePath, csv);
+            File.WriteAllText(filePath + "IndexedStrings.csv", csv);
 
             //using (StreamWriter sWriter = new StreamWriter(filePath))
             //{
@@ -150,7 +274,6 @@ namespace IndexingStringsFromSite_Form
 
         private void BtnToXmlFile_Click(object sender, EventArgs e)
         {
-            string filePath = @"C:\Users\User\Desktop\IndexedStrings.xml";
             XmlWriterSettings xmlSettings = new XmlWriterSettings()
             {
                 Indent = true,
@@ -158,11 +281,12 @@ namespace IndexingStringsFromSite_Form
                 OmitXmlDeclaration = true
             };
 
-            using (XmlWriter xmlWriter = XmlWriter.Create(filePath, xmlSettings))
+            using (XmlWriter xmlWriter = XmlWriter.Create(filePath + "IndexedStrings.xml", xmlSettings))
             {
                 XElement root = new XElement("Root",
                     from keyValue in stringsCountsDict
-                    select new XElement("element", $"{keyValue.Key} repeats {keyValue.Value} times")
+                                        .OrderByDescending(element => element.Value)
+                    select new XElement(keyValue.Key, keyValue.Value)
                 );
                 root.Save(xmlWriter);
             }
@@ -172,13 +296,8 @@ namespace IndexingStringsFromSite_Form
 
         private void BtnToJsonFile_Click(object sender, EventArgs e)
         {
-            File.WriteAllText(@"C:\Users\User\Desktop\IndexedStrings.json", JsonConvert.SerializeObject(stringsCountsDict));
-
-            using (StreamWriter file = File.CreateText(@"C:\Users\User\Desktop\IndexedStrings.json"))
-            {
-                JsonSerializer serializer = new JsonSerializer();
-                serializer.Serialize(file, stringsCountsDict.OrderByDescending(element => element.Value));
-            }
+            File.WriteAllText(filePath + "IndexedStrings.json",
+                JsonConvert.SerializeObject(stringsCountsDict, Newtonsoft.Json.Formatting.Indented));
 
             MessageBox.Show("Information saved to a file IndexedStrings.json");
         }
